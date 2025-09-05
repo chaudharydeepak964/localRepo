5eaab406fea5966d0abba0422e3a26fc9e7872e0
chaudharydeepak964 <chaudharydeepak964@gmail.com>
2025-09-05 21:07:48 +0530

    Use paramiko and Made Connection

--- Filter.py
+++ Filter.py
@@ -2,6 +2,25 @@
 import pandas as pd
 import numpy as np
 import dateandtime
+import paramiko
+
+
+print("Host Connection")
+hostname="0.0.0.0"
+username= "Admin"
+password="Admin"
+port=22
+
+ssh=paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname=hostname,username=username,password=password,port=port)
+sftp=ssh.open_sftp()
+print("Host Connection done")
+
+sftp.chdir(r'\path_remote directory')
+sftp.get('report.csv',r'\path_local_directory\report.csv')
+
+Print("File Downloaded")
 
 df=pd.read_csv(r'F:\GIThUB\GitDemo\My-First-GitHub-Repo\LocalRepo\RAW\U_Full_Link_Report_05_10_2024.csv',skiprows=5,encoding='latin1')
 print(df)
